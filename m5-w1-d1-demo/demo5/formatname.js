5e864afecff42b8e3133dc140a2851edd322c288
YING LIANG <y.liang.168@westcliff.edu>
2023-05-01 21:34:00 -0500

    commit

--- m5-w1-d1-demo/demo5/formatname.js
+++ m5-w1-d1-demo/demo5/formatname.js
@@ -1,5 +1,14 @@
 //add function here
-  
+  function formtName(user){
+    return "first name is "+ user.firstName + ' '+ user.address + " "+ user.lastName;
+  }
+
+  const user= {
+    firstName:"West",
+    lastNameL: "Cliff",
+   
+  };
+
   const element = <h1>Hello, {formatName(user)}!</h1>;
   
   ReactDOM.render(element, document.getElementById('root'));
